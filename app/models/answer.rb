e9260b85fb66746fd28b66d67f6b17d34ea6331c
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-12 21:38:25 +0200

    Added models Question and Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
-class Answer
+class Answer < ActiveRecord::Base
+  belongs_to :question
+
+  validates :content, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
-class Question
+class Question < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, presence: true
+
+  has_many :answers, dependent: :destroy
 end
