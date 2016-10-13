b5abbaa925dbf1d6068ab8ab19947a8a0d8ab7d8
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-13 12:21:04 +0200

    Removed unnecessary index definition in Answer migration

--- db/migrate/20161012082624_create_answers.rb
+++ db/migrate/20161012082624_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :content
-      t.references :question, index: true, foreign_key: true
+      t.references :question
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -23,8 +22,6 @@ ActiveRecord::Schema.define(version: 20161012082624) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
-
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
@@ -32,5 +29,4 @@ ActiveRecord::Schema.define(version: 20161012082624) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "answers", "questions"
 end
