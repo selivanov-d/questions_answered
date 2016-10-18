55379f1e4dfa3475bbd1f7501c1f8282e7b7ebbb
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-18 22:09:10 +0200

    Fixed spec for `delete question` story

--- spec/features/delete_question_spec.rb
+++ spec/features/delete_question_spec.rb
@@ -12,7 +12,8 @@ feature 'Delete question', %q{
   scenario 'Authenticated user deletes his own question' do
     sign_in(user)
 
-    page.driver.submit :delete, question_path(question), {}
+    visit question_path(question)
+    click_on 'Удалить вопрос'
 
     expect(current_path).to eq questions_path
     expect(page).to have_content 'Ваш вопрос удалён'
@@ -24,16 +25,16 @@ feature 'Delete question', %q{
     user2 = create(:user)
     question2 = create(:question, user: user2)
 
-    page.driver.submit :delete, question_path(question2), {}
+    visit question_path(question2)
 
     expect(current_path).to eq question_path(question2)
-    expect(page).to have_content 'Удалить можно только свой вопрос'
+    expect(page).to_not have_link('Удалить вопрос')
   end
 
   scenario 'Non-authenticated user tries to delete question' do
-    page.driver.submit :delete, question_path(question), {}
+    visit question_path(question)
 
-    expect(current_path).to eq new_user_session_path
-    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+    expect(current_path).to eq question_path(question)
+    expect(page).to_not have_link('Удалить вопрос')
   end
 end
