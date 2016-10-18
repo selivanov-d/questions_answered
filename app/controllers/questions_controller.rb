786634a19761d3ffccb6446195a3654d2f305b92
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-18 21:21:04 +0200

    Removed redundant `question_path` helper calls

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -29,7 +29,7 @@ class QuestionsController < ApplicationController
       @question.destroy
       redirect_to questions_path, notice: 'Ваш вопрос удалён'
     else
-      redirect_to question_path(@question), notice: 'Удалить можно только свой вопрос'
+      redirect_to @question, notice: 'Удалить можно только свой вопрос'
     end
   end
 
