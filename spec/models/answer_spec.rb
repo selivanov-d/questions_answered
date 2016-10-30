1944f2710d1aba25cde09870f9156eb39180d665
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-10-30 19:54:26 +0100

    Added associations specs for Question and Answer models

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Answer, type: :model do
+RSpec.describe Answer do
   it { should belong_to(:question) }
   it { should belong_to(:user) }
+  it { should have_many(:attachments).dependent(:destroy) }
 
   it { should validate_presence_of :content }
   it { should validate_length_of(:content).is_at_least(10) }
   it { should validate_uniqueness_of(:best).scoped_to(:question_id) }
+
+  it { should accept_nested_attributes_for :attachments }
 
   describe '#mark_as_best makes an answer the best answer for a question' do
     let(:user) { create(:user) }
