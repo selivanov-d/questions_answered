532de3aaadde98490c6c8195072ebb3f388c5b61
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-11-01 20:05:35 +0100

    Added options for `accepts_nested_attributes_for` method for Answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   has_many :attachments, as: :attachable, dependent: :destroy
 
-  accepts_nested_attributes_for :attachments
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   validates :content, presence: true, length: { minimum: 10 }
   validates :best, uniqueness: { scope: :question_id }, if: :best
