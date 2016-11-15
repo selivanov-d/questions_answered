bcdaacc0554c8e9b870f7be65ee1ad97e841aa77
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-11-15 22:14:44 +0400

    Removed invalid `optional` parameter from Vote association declaration

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ApplicationRecord
-  belongs_to :votable, optional: true, polymorphic: true
+  belongs_to :votable, polymorphic: true
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: [:votable_type, :votable_id] }
