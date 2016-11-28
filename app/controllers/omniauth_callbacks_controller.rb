020470965eba43ea1cab742d7c82536afdc5334e
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-11-28 18:55:11 +0400

    Refactored OmniauthCallbacksController

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,28 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :get_auth_hash
+  before_action :find_user
+  before_action :process_authorization
+
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
   end
 
   def twitter
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
+  end
+
+  private
+
+  def get_auth_hash
+    @auth = request.env['omniauth.auth']
+  end
+
+  def find_user
+    @user = User.find_for_oauth(@auth)
+  end
+
+  def process_authorization
+    if @user && @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: @auth.provider.capitalize) if is_navigational_format?
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
+    return if auth.nil?
+
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
     return authorization.user if authorization
 
