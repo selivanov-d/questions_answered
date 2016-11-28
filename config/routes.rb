7f30ab8c2f3ba95e83f1906d73d8e21f85675b10
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-11-28 16:24:00 +0400

    Added static pages with website policy and terms of service

--- config/routes.rb
+++ config/routes.rb
@@ -30,4 +30,7 @@ Rails.application.routes.draw do
   resources :attachments, only: [:destroy]
 
   mount ActionCable.server => '/cable'
+
+  get 'terms' => 'pages#terms'
+  get 'policy' => 'pages#policy'
 end
