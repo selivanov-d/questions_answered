db59c1f35cf487c284d238b6761355b9f7aa1887
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-11-30 11:27:05 +0400

    Added default CanCan authorization on application level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,18 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
 
   before_action { gon.push({ current_user_id: current_user.try(:id) }) }
+
+  check_authorization unless: :devise_controller?
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
 end
