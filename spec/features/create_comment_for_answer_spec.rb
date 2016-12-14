a3171ec385b6090e26cdef0e168f3a58b6b2de6d
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2016-12-14 15:50:39 +0400

    DRYed up some feature specs

--- spec/features/create_comment_for_answer_spec.rb
+++ spec/features/create_comment_for_answer_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples/features/comment_ability'
 
 feature 'Create comment for answer', %q{
   In order to give additional information for an answer
@@ -6,117 +7,9 @@ feature 'Create comment for answer', %q{
   I want to be able to leave comments
 } do
 
-  let(:question) { create(:question) }
-  let!(:answer) { create(:answer, question: question) }
-
-  context 'Authenticated user' do
-    let(:user) { create(:user) }
-
-    before :each do
-      sign_in(user)
-      visit question_path(question)
-    end
-
-    scenario 'creates comment with valid data', js: true do
-      within ".js-answer[data-answer-id='#{answer.id}']" do
-        find('.js-new-comment-trigger').click
-
-        fill_in 'comment_content', with: 'New comment for answer test content'
-
-        click_on 'Сохранить комментарий'
-      end
-
-      expect(current_path).to eq question_path(question)
-
-      within '.js-comments-for-answer-list' do
-        expect(page).to have_content('New comment for answer test content')
-      end
-    end
-
-    scenario 'creates comment with invalid data', js: true do
-      within ".js-answer[data-answer-id='#{answer.id}']" do
-        find('.js-new-comment-trigger').click
-
-        fill_in 'comment_content', with: 'no_text'
-
-        click_on 'Сохранить комментарий'
-      end
-
-      expect(current_path).to eq question_path(question)
-
-      within '.js-comments-for-question-list' do
-        expect(page).to_not have_content('no_text')
-      end
-    end
-  end
-
-  context 'Non-authenticated user' do
-    scenario 'tries to create comment', js: true do
-      visit question_path(question)
-
-      within ".js-answer[data-answer-id='#{answer.id}']" do
-        expect(page).to_not have_content('Добавить комментарий')
-      end
-    end
-  end
-
-  context 'All users get new questions in real-time' do
-    before :each do
-      author = create(:user)
-      guest = create(:user)
-
-      Capybara.using_session('authenticated_author_creator') do
-        sign_in(author)
-        visit question_path(question)
-      end
-
-      Capybara.using_session('authenticated_author_reader') do
-        sign_in(author)
-        visit question_path(question)
-      end
-
-      Capybara.using_session('authenticated_guest') do
-        sign_in(guest)
-        visit question_path(question)
-      end
-
-      Capybara.using_session('non_authenticated_guest') do
-        visit question_path(question)
-      end
-
-      Capybara.using_session('authenticated_author_creator') do
-        within ".js-answer[data-answer-id='#{answer.id}']" do
-          find('.js-new-comment-trigger').click
-
-          fill_in 'comment_content', with: 'New comment for question test content'
-
-          click_on 'Сохранить комментарий'
-        end
-      end
-    end
-
-    scenario 'authenticated guest', js: true do
-      Capybara.using_session('authenticated_guest') do
-        within ".js-answer[data-answer-id='#{answer.id}'] .js-comments-for-answer-list" do
-          expect(page).to have_content 'New comment for question test content'
-        end
-      end
-    end
-
-    scenario 'non-authenticated guest', js: true do
-      Capybara.using_session('non_authenticated_guest') do
-        within ".js-answer[data-answer-id='#{answer.id}'] .js-comments-for-answer-list" do
-          expect(page).to have_content 'New comment for question test content'
-        end
-      end
-    end
-
-    scenario 'authenticated author as reader', js: true do
-      Capybara.using_session('authenticated_author_reader') do
-        within ".js-answer[data-answer-id='#{answer.id}'] .js-comments-for-answer-list" do
-          expect(page).to have_content 'New comment for question test content'
-        end
-      end
-    end
+  it_behaves_like 'comment ability' do
+    let(:question) { create(:question) }
+    let!(:commentable) { create(:answer, question: question) }
+    let(:commentable_page) { question_path(question) }
   end
 end
--- spec/features/vote_for_question_spec.rb
+++ spec/features/vote_for_question_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared_examples/features/vote_ability'
 
 feature 'Voting for a question', %q{
   In order to praise or to criticize a question
@@ -6,102 +7,8 @@ feature 'Voting for a question', %q{
   I want to be able to give it my vote
 } do
 
-  let(:question) { create(:question) }
-
-  context 'Authenticated user' do
-    context 'not his question' do
-      let(:user) { create(:user) }
-
-      before :each do
-        sign_in(user)
-        visit question_path(question)
-      end
-
-      scenario 'upvotes a question', js: true do
-        within '.js-question-vote-control' do
-          click_on('+1')
-
-          expect(page).to_not have_content('+1')
-          expect(page).to_not have_content('-1')
-          expect(page).to have_content('Unvote')
-        end
-
-        within '.js-question-vote-count' do
-          expect(page).to have_content('1')
-        end
-      end
-
-      scenario 'downvotes a question', js: true do
-        within '.js-question-vote-control' do
-          click_on('-1')
-
-          expect(page).to_not have_content('+1')
-          expect(page).to_not have_content('-1')
-          expect(page).to have_content('Unvote')
-        end
-
-        within '.js-question-vote-count' do
-          expect(page).to have_content('-1')
-        end
-      end
-
-      scenario 'removes his vote from a question', js: true do
-        within '.js-question-vote-control' do
-          click_on('+1')
-          click_on('Unvote')
-
-          expect(page).to have_content('-1')
-          expect(page).to have_content('+1')
-          expect(page).to_not have_content('Unvote')
-        end
-
-        within '.js-question-vote-count' do
-          expect(page).to have_content('0')
-        end
-      end
-    end
-
-    context 'his own question' do
-      before :each do
-        sign_in(question.user)
-        visit question_path(question)
-      end
-
-      scenario 'tries to upvote a question', js: true do
-        within '.js-question-voting' do
-          expect(page).to_not have_content('+1')
-        end
-      end
-
-      scenario 'tries to downvote a question', js: true do
-        within '.js-question-voting' do
-          expect(page).to_not have_content('-1')
-        end
-      end
-
-      scenario 'tries to remove his vote from a question', js: true do
-        within '.js-question-voting' do
-          expect(page).to_not have_content('Unvote')
-        end
-      end
-    end
-  end
-
-  context 'Non-authenticated user' do
-    before :each do
-      visit question_path(question)
-    end
-
-    scenario 'tries to upvote a question', js: true do
-      within '.js-question-voting' do
-        expect(page).to_not have_content('+1')
-      end
-    end
-
-    scenario 'tries to downvote a question', js: true do
-      within '.js-question-voting' do
-        expect(page).to_not have_content('-1')
-      end
-    end
+  it_behaves_like 'vote ability' do
+    let(:votable) { create(:question) }
+    let(:votable_page) { question_path(votable) }
   end
 end
