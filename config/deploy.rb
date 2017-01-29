ee3abcd0446347f84a9728813277b3ec6b4f60ec
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2017-01-29 21:37:39 +0400

    Capistrano added

---
+++ config/deploy.rb
@@ -0,0 +1,42 @@
+# config valid only for current version of Capistrano
+lock '3.7.2'
+
+set :application, 'questions_answered'
+set :repo_url, 'git@github.com:selivanov-d/questions_answered.git'
+set :branch, 'lesson-20'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/questions_answered'
+set :deploy_user, 'deployer'
+
+set :rbenv_ruby, '2.3.1'
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml .env}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+namespace :deploy do
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+end
