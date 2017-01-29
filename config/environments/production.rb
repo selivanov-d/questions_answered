560998f1ea5d76e6d840abcac91a05034f6931f3
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2017-01-29 23:02:15 +0400

    Added allowed_request_origins option for ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [ 'http://82.196.11.135' ]
 end
