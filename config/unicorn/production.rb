78922d58abfec2f436066b753b7d21006a8a05cc
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2017-02-02 20:28:57 +0400

    Attempt to fix nginx 502 error 2

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,7 +4,7 @@ working_directory "#{app_path}/current"
 pid "#{app_path}/current/tmp/pids/unicorn.pid"
 
 # listen
-listen '/var/sockets/unicorn.questions_answered.sock', backlog: 64
+listen "#{app_path}/shared/tmp/sockets/unicorn.questions_answered.sock", backlog: 64
 
 # logging
 stderr_path 'log/unicorn.stderr.log'
