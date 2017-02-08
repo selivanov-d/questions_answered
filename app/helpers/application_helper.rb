c6dd54671631c620eddcdc39ca30e558a78211bb
Dmitrii Selivanov <dmitry.selivanov@castle.co>
2017-02-08 09:54:41 +0400

    Simplified current ability hashing helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def permissions_as_hash_for(user)
-    Digest::MD5.hexdigest(Ability.new(user).permissions.to_s)
+  def current_permissions_as_hash
+    Digest::MD5.hexdigest(current_ability.permissions.to_s)
   end
 end
